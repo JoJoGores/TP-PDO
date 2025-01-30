8129b164b1d6370cdc7e02ff51a699fceda988e1
JoJoGores <jovangores@gmail.com>
2025-01-30 17:17:00 +0100

    séance du 30/01/2025

--- supprimerNationalite.php
+++ supprimerNationalite.php
@@ -1,29 +1,19 @@
 <?php include "header.php";
 include "connexionpdo.php";
-$action=$_GET['num'];
+$num=$_GET['num'];
 
 $req = $monPdo->prepare("delete from nationalite where num = :num");
 $req->bindParam(':num', $num);
 $nb=$req->execute();
 
 
-echo '<div class="container mt-5">';
-echo '<div class="row">
-    <div class="col mt-5">';
 if($nb == 1) {
-    echo '<div class="alert alert-succes" role="alert">
-    La nationalitée a bien été supprimée </div>';
+    $_SESSION['message'] = ['success' => "La nationalitée a bien été supprimée"];
 }else{
-    echo '<div class="alert alert-danger " role="alert">
-    Petit problème : La nationalitée n\'a pas été supprimée </div>';
+    $_SESSION['message'] = ['danger' => "La nationalitée n'a pas été supprimée"];
 }
-?>
-    </div>
-</div>
-<a href="listenationalites.php" class="btn btn-primary"> Revenir a la liste des nationalitées </a>
-</div>
-  
+header('location:listenationalites.php');
+exit();
 
-<?php include "footer.php";
 
 ?>
